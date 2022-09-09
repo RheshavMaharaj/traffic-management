1ebb40b531bfdf82a9615ae986ba1c6666699451
Ahmad Yawari <k_han21@yahoo.com>
2022-09-09 13:51:09 +1000

    added error 404 page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,16 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 import Homepage from './components/Homepage'
+import { Routes, Route } from 'react-router-dom'
+import NotFound from './components/NotFound'
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   )
 }
