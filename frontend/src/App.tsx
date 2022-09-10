f9eab29fe9901e7d2c0e02bf15aa870ec21a2d32
adamKabbara <adamkabbara6@gmail.com>
2022-09-10 13:41:00 +1000

    Added modal to routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
-import Homepage from './components/Homepage'
-import { Routes, Route } from 'react-router-dom'
-import NotFound from './components/NotFound'
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Homepage from './components/Homepage';
+import { Routes, Route } from 'react-router-dom';
+import NotFound from './components/NotFound';
+import ModalPopup from './components/Modal/ModalPopup';
 
 function App() {
   return (
@@ -11,9 +12,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="*" element={<NotFound />} />
+        <Route path="/modal" element={<ModalPopup />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
