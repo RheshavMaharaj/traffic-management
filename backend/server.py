9fca4f45a10b337226fa932e7a49e1107d7996c5
RheshavMaharaj <rheshav@hotmail.com>
2022-09-16 00:33:07 +1000

    Added routes and dataset based routes, changed to pipenv for package management

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+import csv
 
 app = Flask(__name__)
 
@@ -6,6 +7,24 @@ app = Flask(__name__)
 @app.route("/")
 def health_check():
   return {"Success": "True"}
+
+@app.route("/stations/daily")
+def get_stations_daily():
+  with open('./datasets/road_traffic_counts_hourly_sample.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+      print(row['station_key'], row['daily_total'])
+
+@app.route("/stations", methods=['GET'])
+def get_stations():
+  args = request.args
+  latitude = args.get('latitude')
+  longitude = args.get('longitude')
+
+  with open('./datasets/road_traffic_counts_station_reference.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    return list(reader)
+
 
 
 if __name__ == "__main__":
